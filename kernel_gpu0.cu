33803818b0349397d544d708dbe8f8640712707f
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2020-03-28 15:30:04 +0200

    Updated sparseNN in gpu0

--- kernel_gpu0.cu
+++ kernel_gpu0.cu
@@ -8,13 +8,94 @@
 #define THRESHOLD 0.000001
 #define YMAX 32
 
+#define BLOCK_DIM 1024
+
+__global__ spmspm(CSRMatrix *result, CSRMatrix *A, CSCMatrix *B, float bias) {
+    unsigned int nnzIdx = 0;
+
+
+}
+
+
 void sparseNN(Vector* result, COOMatrix* featureVectors, COOMatrix** layerWeights, float bias, unsigned int numLayers) {
 
 
+    Timer timer;
 
+    // Convert featureVectors to CSR
+    startTime(&timer);
+    CSRMatrix* Y0 = createCSRfromCOO(featureVectors);
+    stopTimeAndPrint(&timer, "Convert feature vectors to CSR");
 
+    // Convert layer weights to CSC
+    startTime(&timer);
+    CSCMatrix* W[numLayers];
+    for(unsigned int layer = 0; layer < numLayers; ++layer) {
+        W[layer] = createCSCfromCOO(layerWeights[layer]);
+    }
+    stopTimeAndPrint(&timer, "Convert weights to CSR");
 
+    // Double buffers
+    startTime(&timer);
+    CSRMatrix *tmp = createEmptyCSR(Y0->numRows, Y0->numCols, 2*Y0->nnz);
+    CSRMatrix *inBuffer  = Y0;
+    CSRMatrix *outBuffer = tmp;
+    stopTimeAndPrint(&timer, "Allocate temporary buffer");
+        
+    // Allocate memory on GPU
+    CSRMatrix *inBuffer_d;
+    CSRMatrix *outBuffer_d;
+    CSCMatrix *W_d;
 
+    cudaMalloc((void **) &inBuffer_d, sizeof(CSRMatrix));
+    cudaMalloc((void **) &outBuffer_d, sizeof(CSRMatrix));
+    cudaMalloc((void **) &W_d, sizeof(CSCMatrix));
+    
+
+    // Loop over layers
+    for(unsigned int layer = 0; layer < numLayers; ++layer) {
+
+        // Configurations
+        const unsigned int threadsPerBlock = BLOCK_DIM;
+        const unsigned int blocksPerGrid = (threadsPerBlock + outBuffer->numRows()*outBuffer->numCols() - 1)/threadsPerBlock;
+
+        // Copy data to gpu
+        cudaMemcpy(inBuffer_d, inBuffer, sizeof(CSRMatrix), cudaMemcpyHostToDevice);
+        cudaMemcpy(outBuffer_d, outBuffer, sizeof(CSRMatrix), cudaMemcpyHostToDevice);
+        cudaMemcpy(W_d, W[layer], sizeof(CSRMatrix), cudaMemcpyHostToDevice);
+
+        // SpMSpM
+        printf("Computing layer %u (SpMSpM)", layer);
+        startTime(&timer);
+        // spmspm(outBuffer, inBuffer, W[layer], bias);
+        spmspm <<< blocksPerGrid, threadsPerBlock >>>(outBuffer_d, inBuffer_d, W_d, bias);
+        stopTimeAndPrint(&timer, "");
+
+        // Swap buffers
+        CSRMatrix *t = inBuffer;
+        inBuffer = outBuffer;
+        outBuffer = t;
+
+        
+    }
+    // Free data on GPU
+    cudaFree(inBuffer_d);
+    cudaFree(outBuffer_d);
+    cudaFree(W_d);
+
+    // Find nonzero rows
+    startTime(&timer);
+    findNonzeroRows(result, inBuffer);
+    stopTimeAndPrint(&timer, "Find nonzero rows");
+
+    // Free buffers
+    startTime(&timer);
+    freeCSR(Y0);
+    for(unsigned int layer = 0; layer < numLayers; ++layer) {
+        freeCSC(W[layer]);
+    }
+    freeCSR(tmp);
+    stopTimeAndPrint(&timer, "Deallocate memory");
 
 }
 
