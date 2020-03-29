e0930988d8144a85c1f124d72d4f3e12925338bb
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2020-03-29 20:09:43 +0300

    Updated spmspm

--- kernel_gpu0.cu
+++ kernel_gpu0.cu
@@ -11,11 +11,100 @@
 #define BLOCK_DIM 1024
 
 __global__ spmspm(CSRMatrix *result, CSRMatrix *A, CSCMatrix *B, float bias) {
-    unsigned int nnzIdx = 0;
 
+    unsigned int r = blockDim.x*blockIdx.x + threadIdx.x;
+    unsigned int nnzIdx; // Number of non zero indexes in Result
+    if(r > 1){
+        nnzIdx = result->rowPtrs[r - 1];
+    } else {
+        nnzIdx = 0;
+    }
+
+    if(r < A->numRows ){
+        unsigned int rowPtrA = A->rowPtrs[r]; // Index of the current rowPtrs element
+        unsigned int nnzA = A->rowPtrs[r + 1] - rowPtrA;  // Number of non zero elements in A
+
+        if(nnzA > 0){
+            unsigned int *colIdxsA = A->colIdxs + rowPtrA;
+            float *valueA = A->values + rowPtrA;
+
+            // Loop over B columns
+            for(unsigned int c = 0; c < B->numCols; ++c){
+                unsigned int colPtrB = B->colPtrs[c];
+                unsigned int nnzB = B->colPtrs[c + 1] = colPtrB;
+
+                if(nnzB > 0){
+                    unsigned int *rowsIdxsB = B->rowsIdxs + colPtrB;
+                    float *valueB = B->values + colPtrB;
+
+                    // Loop and find intersection
+                    float sum = 0;
+                    unsigned int ia = 0;
+                    unsigned int ib = 0;
+
+                    // Loop over segment of non zero elements in the row of A and col of B
+                    while(ia < nnzA && ib < nnzzB){
+                        unsigned int colIdx = colIdxsA[ia];
+                        unsigned int rowIdx = rowsIdxsB[ib];
+                        if(colIdx < rowIdx) {
+                            ia++;
+                        } else if(colIdx > rowIdx) {
+                            ib++;
+                        } else {
+                            sum += valueA[ia]*valueB[ib];
+                            ia++;
+                            ib++;
+                        }
+                    }
+
+                    // Sync threads
+                    __syncthreads();
+
+                    // Write to Result
+                    if(sum > THRESHOLD || sum < -THRESHOLD) {
+                        sum += bias;
+
+                        __syncthreads();
+                        
+                        //Remove negative and zero values
+                        if(sum > 0) {
+                            if(sum>YMAX) {
+                                sum = YMAX;
+                            }
+                            if(nnzIdx >= result->capacity) {
+                                expandCSRCapacity(result, 2*result->capacity);
+                            }
+                            result->colIdxs[nnzIdx] = c; // FIX HERE
+                            result->values[nnzIdx] = sum;
+                            ++nnzIdx;
+                        }    
+                    }
+                }
+            }
+        }
+        result->rowPtrs[r + 1] = nnzIdx;        
+    }
+    // Atomic add (?)
+    atomicAdd(&result->nnz, nnzIdx);
+    // result->nnz = nnzIdx;
 
 }
 
+void findNonzeroRows(Vector* v, CSRMatrix* A) {
+    unsigned int nnz = 0;
+    for(unsigned int r = 0; r < A->numRows; ++r) {
+        unsigned int rowPtrA = A->rowPtrs[r];
+        unsigned int nnzA = A->rowPtrs[r + 1] - rowPtrA;
+        if(nnzA > 0) {
+            if(nnz >= v->capacity) {
+                expandVectorCapacity(v, 2*v->capacity);
+            }
+            v->data[nnz] = r;
+            ++nnz;
+        }
+    }
+    v->nnz = nnz;
+}
 
 void sparseNN(Vector* result, COOMatrix* featureVectors, COOMatrix** layerWeights, float bias, unsigned int numLayers) {
 
@@ -57,7 +146,7 @@ void sparseNN(Vector* result, COOMatrix* featureVectors, COOMatrix** layerWeight
 
         // Configurations
         const unsigned int threadsPerBlock = BLOCK_DIM;
-        const unsigned int blocksPerGrid = (threadsPerBlock + outBuffer->numRows()*outBuffer->numCols() - 1)/threadsPerBlock;
+        const unsigned int blocksPerGrid = (threadsPerBlock + outBuffer->numRows() - 1)/threadsPerBlock;
 
         // Copy data to gpu
         cudaMemcpy(inBuffer_d, inBuffer, sizeof(CSRMatrix), cudaMemcpyHostToDevice);
