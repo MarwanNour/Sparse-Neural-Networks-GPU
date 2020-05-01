b317f57d3e8315c47c29c4e5d7647bda9f67a7e2
aliamir51 <aliamir51.aa@gmail.com>
2020-05-01 15:08:05 +0300

    commit

--- kernel_gpu1.cu
+++ kernel_gpu1.cu
@@ -235,6 +235,15 @@ void sparseNN(Vector* result, COOMatrix* featureVectors, COOMatrix** layerWeight
         // Convert COO to CSR
         startTime(&timer);
         convertCOOtoCSR(Yout, Yin);
+        if(layer==0){
+            int max=0
+            for(int i=0;i<Yout->numRows;++i){
+                if(Yout->rowPtrs[i+1]-Yout->rowPtrs[i]>max){
+                    max=Yout->rowPtrs[i+1]-Yout->rowPtrs[i]>max;
+                }
+            }
+            printf("max elements in row = %d " ,max);
+        }
         stopTimeAndPrint(&timer, "    Converting COO to CSR");
     
 
